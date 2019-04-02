0e642fcb1fd6e078623f3fc7737993a1096ab4cd
Nagornykh Maria <manyaknag@gmail.com>
2019-04-02 09:32:48 +0300

    начало

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="assets/img/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+  import HelloWorld from './components/HelloWorld.vue';
+
+  export default {
+    name: 'app',
+    components: {
+      HelloWorld,
+    },
+  };
+</script>
+
+<style src='./assets/stylesheets/App.css'></style>
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  css: {
+    modules: true,
+    sourceMap: true,
+  },
+  productionSourceMap: false,
+};

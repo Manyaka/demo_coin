054aae57342d5fed209c96cf10c7e5604f8d3619
Nagornykh Maria <manyaknag@gmail.com>
2019-04-02 16:05:02 +0300

    список

--- src/components/List.vue
+++ src/components/List.vue
@@ -3,7 +3,7 @@
     <ul>
       <li v-for="(value, key) in pairs" v-bind:key="key">
         <label v-bind:for="`id_${key}`">
-          <input type="checkbox" v-bind:id="`id_${key}`" v-on:change="selectItem()">
+          <input type="checkbox" v-bind:id="`id_${key}`" v-on:change="toggleItem(key)" />
           {{ key }}
         </label>
       </li>
@@ -13,7 +13,7 @@
 
 <script>
   import masha from '../pairs.json';
-  console.log(Object.keys(masha.pairs).length);
+  // console.log(Object.keys(masha.pairs).length);
 
   export default {
     name: 'List',
@@ -22,11 +22,28 @@
         pairs: masha.pairs,
       };
     },
+    //есть ещё вариант перенести данные в computed, чтобы вью не реактивила их
+    //типа для убыстрения перфоманса, хотя статистика показала, что перфоманс
+    //не увеличился
+    /*computed: {
+     pairs: () => masha.pairs,
+     },*/
+    created() {
+      this.selectedItems = [];
+    },
     methods: {
-      selectItem() {
-        // return
-      }
-    }
+      toggleItem(key) {
+        let index = this.selectedItems.indexOf(key);
+
+        if (index !== -1) {
+          this.selectedItems.splice(index, 1);
+        } else {
+          this.selectedItems.push(key);
+        }
+
+        console.log(this.selectedItems);
+      },
+    },
   };
 </script>
 

c57c955963b7517f0495450d5aa1617765bc66e3
Nagornykh Maria <manyaknag@gmail.com>
2019-07-11 10:05:36 +0300

    обновляшки

--- src/api.js
+++ src/api.js
@@ -55,3 +55,19 @@ export function getTicker(pair) {
     // Here's a list of repos!
     console.log(data);
   });*/
+
+/*fetch('./api/some.json')
+  .then(function(response) {
+    if (response.status !== 200) {
+      console.log('Looks like there was a problem. Status Code: ' + response.status);
+      return;
+    }
+
+    // Examine the text in the response
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  })
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });*/
